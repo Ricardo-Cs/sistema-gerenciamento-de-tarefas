3bb0e844966799065415a50db5bce5810293e48e
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-06-08 17:25:13 -0500

    Cria component de Login e arquivo de rotas

--- sistema-app/src/app/app.module.ts
+++ sistema-app/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './component/login-page/login-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
